f79b402a0b13e61faca9d647001a13efb7d28eaa
Mansoor Khan <mansoorKh1098@gmail.com>
2023-09-16 14:41:16 +0530

    🚧 feat:display platform Icons

--- src/components/GamesCards.tsx
+++ src/components/GamesCards.tsx
@@ -4,11 +4,13 @@ import {
   CardHeader,
   Heading,
   Image,
+  Text,
   useColorMode,
 } from "@chakra-ui/react";
 import React from "react";
 
 import { Game } from "../hook/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -18,10 +20,17 @@ const GamesCards = ({ game }: Props) => {
   const { toggleColorMode, colorMode } = useColorMode();
   console.log(colorMode);
   return (
-    <Card overflow={"hidden"} borderRadius={10}>
+    <Card
+      overflow={"hidden"}
+      borderRadius={10}
+      boxShadow={colorMode == "dark" ? "base" : "xl"}
+    >
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaAndroid,
+  FaLinux,
+  FaApple,
+  FaXbox,
+} from "react-icons/fa";
+
+import { IconType } from "react-icons";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hook/useGames";
+import { HStack, Icon, Text, useColorMode } from "@chakra-ui/react";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  const iconMap: { [key: string]: IconType } = {
+    //name: PlayStation
+    //slug: playstation
+
+    pc: FaWindows,
+    xbox: FaXbox,
+    apple: FaApple,
+    playstation: FaPlaystation,
+    linux: FaLinux,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <>
+      <HStack marginY={1}>
+        {" "}
+        //theme.space margin will calculate on this bases like value 1 //default
+        value in chakra 4px
+        {platforms.map((platform) => (
+          <Icon
+            as={iconMap[platform.slug]}
+            color={colorMode === "dark" ? "teal.400" : "green.700"}
+          />
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
