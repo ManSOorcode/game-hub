a8cffc64c28b5a13b896e2b82d1dba4a448a2e44
Mansoor Khan <mansoorKh1098@gmail.com>
2023-09-16 21:52:20 +0530

    💄 feat: refactor: remove duplicate style

--- src/components/GamesCards.tsx
+++ src/components/GamesCards.tsx
@@ -14,6 +14,7 @@ import { Game } from "../hook/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImagesUrl from "../services/image-crops";
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
@@ -23,23 +24,20 @@ const GamesCards = ({ game }: Props) => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
-    <Card
-      width={"300px"}
-      overflow={"hidden"}
-      borderRadius={10}
-      boxShadow={colorMode == "dark" ? "base" : "xl"}
-    >
-      <Image src={getCroppedImagesUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card boxShadow={colorMode == "dark" ? "base" : "xl"}>
+        <Image src={getCroppedImagesUrl(game.background_image)} />
+        <CardBody>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <HStack justifyContent={"space-between"}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
